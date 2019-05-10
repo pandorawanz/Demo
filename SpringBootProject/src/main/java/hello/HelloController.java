1c841a94cf756ecb3cee8f28bbce8dd258bd4dd3
wanz <lzh60587@gmail.com>
2019-05-10 22:50:41 +0800

    SpringBootProject v0.2

--- SpringBootProject/src/main/java/hello/HelloController.java
+++ SpringBootProject/src/main/java/hello/HelloController.java
@@ -1,8 +1,11 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class HelloController {
@@ -10,5 +13,18 @@ public class HelloController {
     public String index(@RequestParam(value = "user", defaultValue = "wanz") String id) {
         // 后端渲染
         return String.format("<html><body>我们的主页欢迎%s~</body></html>", id);
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestParam("username")String username,
+                        @RequestParam("password")String password) {
+        System.out.println("username:" + username + "\npassword:" + password);
+        return "";
+    }
+
+    @RequestMapping("/whoami")
+    public String whoami() {
+        // 后端渲染
+        return UUID.randomUUID().toString();
     }
 }
